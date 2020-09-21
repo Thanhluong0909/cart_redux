7ecfe67d21b0520eb65f5bed42afe205fddf340a
thanh luong <thanhluong0909@gmail.com>
2020-09-21 11:02:19 +0700

    'create-router'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ProductsContainer from './containers/ProductsContainer';
-import CartContainer from './containers/CartContainer';
-import Messagecontainer from './containers/Messagecontainer';
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
+import About from './components/About';
+import Home from './components/Home';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import ProductsContainer from './containers/ProductsContainer';
+// import CartContainer from './containers/CartContainer';
+// import Messagecontainer from './containers/Messagecontainer';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div>
+
+            <Router>
+                <div className="App">
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <a href="/" className="nav-item nav-link ">trang chủ </a>
+                                <a href="/about" className="nav-item nav-link">liên hệ</a>
+                            </div>
+                        </div>
+                    </nav>
+                    
+                    {/* nội dung  */}
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    {/* <div>
                     <Header />
                     <main id="mainContainer">
                         <div className="container">
@@ -19,11 +36,11 @@ class App extends Component {
                         </div>
                     </main>
                     <Footer />
+                </div> */}
                 </div>
-            </div>
+            </Router>
         );
     }
-
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                đây là trang chủ
+            </div>
+        );
+    }
+
+}
+
+export default Home;
